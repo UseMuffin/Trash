bf2972a0662a98b676c299171c81ee8560e89384
Yves P <havokinspiration@gmail.com>
2018-03-06 08:52:06 +0100

    Prepare the plugin for CakePHP 3.6

--- src/Model/Behavior/TrashBehavior.php
+++ src/Model/Behavior/TrashBehavior.php
@@ -48,7 +48,7 @@ class TrashBehavior extends Behavior
     public function initialize(array $config)
     {
         if (!empty($config['events'])) {
-            $this->config('events', $config['events'], false);
+            $this->setConfig('events', $config['events'], false);
         }
     }
 
@@ -61,10 +61,10 @@ class TrashBehavior extends Behavior
     public function implementedEvents()
     {
         $events = [];
-        if ($this->config('events') === false) {
+        if ($this->getConfig('events') === false) {
             return $events;
         }
-        foreach ((array)$this->config('events') as $eventKey => $event) {
+        foreach ((array)$this->getConfig('events') as $eventKey => $event) {
             if (is_numeric($eventKey)) {
                 $eventKey = $event;
                 $event = null;
@@ -75,7 +75,7 @@ class TrashBehavior extends Behavior
             if (!is_array($event)) {
                 throw new \InvalidArgumentException('Event should be string or array');
             }
-            $priority = $this->config('priority');
+            $priority = $this->getConfig('priority');
             if (!array_key_exists('callable', $event) || $event['callable'] === null) {
                 list(, $event['callable']) = pluginSplit($eventKey);
             }
@@ -105,7 +105,7 @@ class TrashBehavior extends Behavior
 
         $event->stopPropagation();
 
-        $event->subject()->dispatchEvent('Model.afterDelete', [
+        $event->getSubject()->dispatchEvent('Model.afterDelete', [
             'entity' => $entity,
             'options' => $options
         ]);
@@ -123,7 +123,7 @@ class TrashBehavior extends Behavior
      */
     public function trash(EntityInterface $entity, array $options = [])
     {
-        $primaryKey = (array)$this->_table->primaryKey();
+        $primaryKey = (array)$this->_table->getPrimaryKey();
 
         if (!$entity->has($primaryKey)) {
             throw new RuntimeException();
@@ -236,7 +236,7 @@ class TrashBehavior extends Behavior
         $data = [$this->getTrashField(false) => null];
 
         if ($entity instanceof EntityInterface) {
-            if ($entity->dirty()) {
+            if ($entity->isDirty()) {
                 throw new RuntimeException('Can not restore from a dirty entity.');
             }
             $entity->set($data, ['guard' => false]);
@@ -261,14 +261,14 @@ class TrashBehavior extends Behavior
         foreach ($this->_table->associations() as $association) {
             if ($this->_isRecursable($association, $this->_table)) {
                 if ($entity === null) {
-                    $result += $association->target()->cascadingRestoreTrash(null, $options);
+                    $result += $association->getTarget()->cascadingRestoreTrash(null, $options);
                 } else {
-                    $foreignKey = (array)$association->foreignKey();
-                    $bindingKey = (array)$association->bindingKey();
+                    $foreignKey = (array)$association->getForeignKey();
+                    $bindingKey = (array)$association->getBindingKey();
                     $conditions = array_combine($foreignKey, $entity->extract($bindingKey));
 
                     foreach ($association->find('withTrashed')->where($conditions) as $related) {
-                        if (!$association->target()->cascadingRestoreTrash($related, ['_primary' => false] + $options)) {
+                        if (!$association->getTarget()->cascadingRestoreTrash($related, ['_primary' => false] + $options)) {
                             $result = false;
                         }
                     }
@@ -301,10 +301,10 @@ class TrashBehavior extends Behavior
      */
     public function getTrashField($aliased = true)
     {
-        $field = $this->config('field');
+        $field = $this->getConfig('field');
 
         if (empty($field)) {
-            $columns = $this->_table->schema()->columns();
+            $columns = $this->_table->getSchema()->columns();
             foreach (['deleted', 'trashed'] as $name) {
                 if (in_array($name, $columns, true)) {
                     $field = $name;
@@ -320,7 +320,7 @@ class TrashBehavior extends Behavior
                 throw new RuntimeException('TrashBehavior: "field" config needs to be provided.');
             }
 
-            $this->config('field', $field);
+            $this->getConfig('field', $field);
         }
 
         if ($aliased) {
@@ -339,10 +339,10 @@ class TrashBehavior extends Behavior
      */
     protected function _isRecursable(Association $association, Table $table)
     {
-        if ($association->target()->hasBehavior('Trash')
+        if ($association->getTarget()->hasBehavior('Trash')
             && $association->isOwningSide($table)
-            && $association->dependent()
-            && $association->cascadeCallbacks()) {
+            && $association->getDependent()
+            && $association->getCascadeCallbacks()) {
             return true;
         }
 
